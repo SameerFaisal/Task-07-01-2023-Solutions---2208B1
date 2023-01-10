cdedb90ec319064f930db6f0377e97eacd0695a8
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-01-10 14:17:25 +0500

    07-01-2023 Task Solutions Updated

---
+++ Task Solution/index.js
@@ -0,0 +1,65 @@
+// let age=+prompt("Enter your age")
+// alert("Your age in days is: "+age*365)
+
+
+// let age=+prompt("Enter your age")
+// alert("Your age in hours is: "+age*8760)
+
+// let num1=+prompt("Enter first number")
+// let num2=+prompt("Enter second number")
+
+// let result=num1-(-num2)
+
+// alert(result)
+
+// let num2=+prompt("Enter a number")
+
+// let result=(num2)**(0.5)
+// alert(result)
+
+// let input=prompt("Enter something")
+// let length=input.length
+
+// alert(input.charAt(0))
+
+
+// let input=prompt("Enter something")
+// alert(input.toUpperCase())
+
+
+// let number=+prompt("Enter a number")
+
+// for(let i=0;i<=100;i++){
+//     if(number%i===0){
+//         document.write(i+"<br>")
+//     }
+// }
+
+
+// let num=0
+// document.write(num)
+// document.write("<br>")
+// document.write(num+3)
+// document.write("<br>")
+// document.write(num)
+// document.write("<br>")
+// document.write(num+7)
+// document.write("<br>")
+// document.write(num+2)
+// document.write("<br>")
+// document.write(num+2)
+// document.write("<br>")
+// document.write(num)
+// document.write("<br>")
+// document.write(num)
+// document.write("<br>")
+// document.write(num)
+// document.write("<br>")
+// document.write(num)
+// document.write("<br>")
+// document.write(num+1)
+
+
+
+
+
